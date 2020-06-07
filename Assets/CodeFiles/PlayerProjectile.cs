bf11ae4a1490b38eb31317a94c707355af87c694
YapYongLiang <yongliangyap2000@gmail.com>
2020-06-07 17:25:59 +0800

    new stuff

---
+++ Assets/CodeFiles/PlayerProjectile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProjectile : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody2D rb2d;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       rb2d.AddForce(-transform.right * speed, ForceMode2D.Force);
+    }
+}
---
+++ Assets/CodeFiles/PlayerShoot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public Transform barrel;
+    
+    public GameObject projectilePrefab;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject projectile = Instantiate(projectilePrefab, barrel.transform.position, barrel.rotation);
+            
+        }
+    }
+}
